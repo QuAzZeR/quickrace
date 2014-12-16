fc3d3e00792a77df5d0cf93e1986d56f76809045
Padungkiet Tamasee <QuAzZeR@Padungkiets-MacBook-Pro.local>
2014-12-16 13:46:35 +0700

    game can play and collider to enemies

--- element.py
+++ element.py
@@ -10,6 +10,7 @@ class Player(object):
         self.x = x
         self.y = y
         self.vx = 5
+        self.rect = Rect(self.x,self.y,self.height,self.width)
 
     def render(self, surface):
         pygame.draw.rect(surface,self.color,pygame.Rect(self.x,self.y,self.height,self.width))
@@ -17,8 +18,44 @@ class Player(object):
     def move_left(self):
         if self.x >= 20:
             self.x-=self.vx
-
+    def get_rect(self):
+        self.rect = Rect(self.x,self.y,self.height,self.width)
+        return self.rect
     def move_right(self):
         if self.x <= 231:
             self.x+=self.vx
+
+class Enemie(object):
+    LEFT_LANE=26
+    MIDDLE_LANE= 126 
+    RIGHT_LANE= 236
+    LEFT_COLOR=pygame.Color('red');
+    MIDDLE_COLOR=pygame.Color('white');
+    RIGHT_COLOR=pygame.Color('green');
+    WIDTH=60
+    HEIGHT=60
+    def __init__(self, lane = 1, y = 0):
+        self.width = Enemie.WIDTH
+        self.height = Enemie.HEIGHT
+        self.y = y
+        if lane == 1:
+            self.x = Enemie.LEFT_LANE
+            self.color=Enemie.LEFT_COLOR
+        elif lane == 2:
+            self.x= Enemie.MIDDLE_LANE
+            self.color=Enemie.MIDDLE_COLOR
+        elif lane == 3:
+            self.x = Enemie.RIGHT_LANE
+            self.color = Enemie.RIGHT_COLOR
+        self.rect = pygame.Rect(self.x,self.y,self.height,self.width)
+
+    def render(self, surface):
+        pygame.draw.rect(surface,self.color,pygame.Rect(self.x,self.y,self.height,self.width))
+    
+    def update(self):
+        self.y+=6
+        self.rect = pygame.Rect(self.x,self.y,self.height,self.width)
+
+    def get_rect(self):
+        return self.rect
 
--- main.py
+++ main.py
@@ -4,26 +4,32 @@ from gamelib import SimpleGame
 from element import *
 class QuickRace(SimpleGame):
     BLACK = pygame.Color('black')
+    
     def __init__(self):
         super(QuickRace, self).__init__('QuickRace', QuickRace.BLACK, window_size = (320,480))
-        self.player = Player()
-        self.player4 = Player(y=340, color = pygame.Color('white'))
-        self.player2 = Player(x=21, y=340, color=pygame.Color('red'))
-        self.player3 = Player(x=236, y = 340, color=pygame.Color('yellow'))
+
     def init(self):
-        super(QuickRace, self).init()
-    
+        super(QuickRace, self).init() 
+        global enemies,player
+        self.isover=False
+        player = Player()
+        enemies = [Enemie(1,-480),Enemie(2,-960),Enemie(3,-1000)]        
+
     def render(self,surface):
-        self.player2.render(surface)
-        self.player3.render(surface)
-        self.player4.render(surface)
-        self.player.render(surface)
+        player.render(surface)
+        for e in enemies:
+            e.render(surface)
+
     def update(self):
         if self.is_key_pressed(K_LEFT):
-            self.player.move_left()
+            player.move_left()
         elif self.is_key_pressed(K_RIGHT):
-            self.player.move_right()
-
+            player.move_right()
+        for e in enemies:
+            if not self.isover:
+                e.update()
+            if player.get_rect().colliderect(e.get_rect()):
+                self.isover=True
 def main():
     game = QuickRace()
     game.run()
